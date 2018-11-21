4de5771a3fdcc02e0e31a304bf3787d957f57ec8
gabrielloyola <gabrielfrml@gmail.com>
2018-11-21 13:53:59 -0200

    Trying to code and decode msg

--- client.py
+++ client.py
@@ -1,32 +1,55 @@
 import socket
 import binascii
-import matplotlib.pyplot as plt
+import pdb
+# import matplotlib.pyplot as plt
 
 HOST = '127.0.0.1'     # Endereco IP do Servidor
-PORT = 5001            # Porta que o Servidor esta
+PORT = 5000            # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 tcp.connect(dest)
 
-print('Para sair use -1\n')
-
-msg = input()
-
-signal = []
-while msg != '-1':
-	binary = ''.join(format(ord(x), 'b') for x in msg)
-	
-	tmp = list(binary)
+def NRZ(binary):
 	coded = []
-	for x in tmp:
+
+	for x in binary:
 		coded.append(x)
 		coded.append(x)
 
-	plt.plot(vet)
-	plt.title("NRZ-RZ")
-	plt.show()
+	return coded
 
-	tcp.send(binary)
-	msg = input()
+def RZ(binary):
+	coded = []
 
-tcp.close()
+	for x in binary:
+		coded.append(x)
+		coded.append(0) # return to zero
+
+	return coded
+
+def main():
+	nrz = raw_input("Digite 1 para nrz e 0 para rz: ")
+
+	print('Para sair use -1\n')
+
+	msg = raw_input()
+
+	while msg != '\x18':
+		binary = ''.join(format(ord(x), 'b') for x in msg)
+
+		coded = NRZ(binary) if nrz else RZ(binary)
+
+		# plt.plot(vet)
+		# plt.title("NRZ")
+		# plt.show()
+
+		str_coded = ''
+		for x in coded: str_coded += x
+
+		tcp.send(str_coded)
+		msg = raw_input()
+
+	tcp.close()
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,26 +1,49 @@
 import socket
+import pdb
 
 HOST = ''              # Endereco IP do Servidor
-PORT = 5001            # Porta que o Servidor esta
+PORT = 5000            # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
 tcp.bind(orig)
 tcp.listen(1)
 
-print("Servidor Iniciado!")
+# Nao varia entre nrz e rz pois considera somente os sinais pares do vetor
+def decodeMsg(coded):
+    binary = ''
 
-while True:
-    con, cliente = tcp.accept()
-    print 'Concetado por', cliente
+    for i, x in enumerate(coded):
+        if i % 2 != 0:
+            binary += x 
+            if i < len(coded) - 1:
+                binary += ' '
+        else: continue
+    
+    return binary
 
+def bin2Msg(binary):
+    bin_chars = binary.split(', ', 7)
+    msg = ''
+    pdb.set_trace()
+    for x in bin_chars: msg += unichr(int(x, 2)) 
+    return msg
+
+def main():
+    print("Servidor Iniciado!")
     while True:
+        con, cliente = tcp.accept()
+        print('Concetado por', cliente)
 
-        coded = con.recv(1024)
-        msg = ''
-        for x in coded.split(' '):
-            msg += str(unichr(int(x, 2)))
-        if not coded: break
-        print(cliente, msg)
-        print(coded)
-    print('Finalizando conexao do cliente', cliente)
-    con.close()
+        while True:
+            coded = con.recv(1024)
+            decoded = decodeMsg(coded)
+            msg = bin2Msg(decoded)
+            if not coded: break
+            print(cliente, msg)
+
+        print('Finalizando conexao do cliente', cliente)
+        con.close()
+
+
+if __name__ == "__main__":
+    main()
